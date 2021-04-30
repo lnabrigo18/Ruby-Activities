cd3b7d9081d470101c9bb3af291366a043851f93
lnabrigo18 <lnabrigo@gmail.com>
2021-05-01 00:27:57 +0800

    activity3_3.rb

---
+++ activity3_3.rb
@@ -0,0 +1,10 @@
+while true
+      print  "Enter a number: "
+      value = gets.chomp
+      if value == "STOP"
+        break
+      end
+      if value == "stop"
+        break
+      end
+    end
