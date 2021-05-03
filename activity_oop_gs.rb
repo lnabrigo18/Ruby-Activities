eda11a7da5b5534f18abdb28f057f84fce9439c2
lnabrigo18 <lnabrigo@gmail.com>
2021-05-03 20:57:12 +0800

    .

--- activity_oop_gs.rb
+++ activity_oop_gs.rb
@@ -1,6 +1,6 @@
 class Profile
     attr_accessor :full_name, :age, :work
-    attr_writer :address
+    attr_reader :address
 
     def initialize(full_name, age, address, work)
         @full_name = full_name
